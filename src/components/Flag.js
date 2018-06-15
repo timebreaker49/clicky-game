d7f837fb07b7871dce5d8f719ea9b218bfb2b428
Wayne Baylor <wcsbaylor@gmail.com>
2018-06-14 23:09:45 -0500

    added flag component and json file to reference flag images

--- src/components/Flag.js
+++ src/components/Flag.js
@@ -3,9 +3,14 @@ import React, { Component } from 'react';
 export default class Flag extends Component {
   render() {
     return (
-      <div className="flag">
+      <img className="flag"
+        id={"flag" + this.props.id}
+        src={this.props.src}
+        alt={this.props.name}
+
+        // onClick={ () => function to handle click this.props.handleClick(this.props.id) }
         { this.props.children }
-      </div>
+      />
     )
   }
 }
